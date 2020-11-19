1786e7a9aca449bed5409ca77657bd5bd88359f5
Pavlo Bogoomolenko <pavlo.bogomolenko@gmail.com>
2020-11-19 15:01:58 +0200

    Implement MoveToFront decore method

--- algorithms-part2-by-rsedgewick/w5/burrows-wheeler/src/main/java/MoveToFront.java
+++ algorithms-part2-by-rsedgewick/w5/burrows-wheeler/src/main/java/MoveToFront.java
@@ -1,23 +1,18 @@
 package main.java;
 
+import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class MoveToFront {
     // 256 extended ASCII characters
-//    private final static int R = 256;
-    private final static int R = 6;
+    private final static int R = 256;
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-//        char[] input = {'C', 'A', 'A', 'A', 'B', 'C', 'C', 'C', 'A', 'C', 'C', 'F'};
-        char[] input = {(char) 2, (char) 0, (char) 0, (char) 0, (char) 1, (char) 2, (char) 2, (char) 2, (char) 0, (char) 2, (char) 2, (char) 5};
+//      char[] input = {(char) 2, (char) 0, (char) 0, (char) 0, (char) 1, (char) 2, (char) 2, (char) 2, (char) 0, (char) 2, (char) 2, (char) 5};
+        char[] input = BinaryStdIn.readString().toCharArray();
 
-        Node root = new Node('\0');
-        Node current = root;
-        for(int i = 1; i < R; i++) {
-            current.next = new Node((char) i);
-            current = current.next;
-        }
+        Node root = getLinkedAsciiNode();
 
         for(int i = 0; i < input.length; i++) {
             char c = input[i];
@@ -42,6 +37,36 @@ public class MoveToFront {
         }
     }
 
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+//        char[] input = {(char) 2, (char) 1, (char) 0, (char) 0, (char) 2, (char) 2, (char) 0, (char) 0, (char) 2, (char) 1, (char) 0, (char) 5};
+        char[] input = BinaryStdIn.readString().toCharArray();
+
+        Node root = getLinkedAsciiNode();
+
+        for(int i = 0; i < input.length; i++) {
+            char c = input[i];
+            int counter = 0;
+            Node node = root;
+            Node prev = root;
+            while (node != null) {
+                if(counter == c) {
+                    StdOut.println((byte) node.value);
+                    if(node == root) {
+                        break;
+                    }
+                    prev.next = node.next;
+                    node.next = root;
+                    root = node;
+                    break;
+                }
+                prev = node;
+                node = node.next;
+                counter++;
+            }
+        }
+    }
+
     private static class Node {
         private char value;
         private Node next;
@@ -51,11 +76,23 @@ public class MoveToFront {
         }
     }
 
+    private static Node getLinkedAsciiNode() {
+        Node root = new Node('\0');
+        Node current = root;
+        for (int i = 1; i < R; i++) {
+            current.next = new Node((char) i);
+            current = current.next;
+        }
+        return root;
+    }
+
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
         if(args[0].equals("-")) {
             MoveToFront.encode();
+        } else if(args[0].equals("+")) {
+            MoveToFront.decode();
         }
     }
 }
